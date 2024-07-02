333a64eede9bfe7cc55af12a500ca57647f26c42
Eduardo Siqueira <eduardoms@id.uff.br>
2024-07-02 17:11:52 -0300

    Adding SwaggerConfig
    run localhost:8080/swagger-ui/index.html

---
+++ src/main/java/com/dac/api/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.dac.api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.companyname.app"))
+                .paths(PathSelectors.regex("/.*"))
+                .build().apiInfo(apiInfoMetaData());
+    }
+
+    private ApiInfo apiInfoMetaData() {
+
+        return new ApiInfoBuilder().title("API CMS")
+                .description("API Endpoint para projeto de da Sociedade Brasileira de Computação")
+                .contact(new Contact("Dev-Team: Eduardo Siqueira / Marcia Yumi / Carol Arêas", "https://www.dev-team.com/", "dev-team@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
+    }
+
+}
